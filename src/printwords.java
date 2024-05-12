9f5325c507f86b95ae77e64d0b9573f167144270
Katie <kf1030@student.cis.edu.hk>
2024-05-12 14:29:55 +0800

    create word grid and display first 3 lines

--- src/printwords.java
+++ src/printwords.java
@@ -4,57 +4,107 @@ import acm.program.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.FileNotFoundException;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class printwords  extends GraphicsProgram implements ActionListener {
-    String words = "I recently discovered I could make fudge with just chocolate chips, sweetened condensed milk, vanilla extract, and a thick pot on slow heat. I tried it with dark chocolate chunks and I tried it with semi-sweet chocolate chips. It's better with both kinds. It comes out pretty bad with just the dark chocolate. The best add-ins are crushed almonds and marshmallows -- what you get from that is Rocky Road. It takes about twenty minutes from start to fridge, and then it takes about six months to work off the twenty pounds you gain from eating it. All things in moderation, friends. All things in moderation.";
+    private static ArrayList<GLabel>[] displayGrid = new ArrayList[500];
+    int gridMax;
+    int cursorX = 0,cursorY = 0;
 
-    private GLabel a[][] = new GLabel[4][21];
-    private int posx=0,posy=0;
-    public void run(){
-        int x=30,y=30,cnt=0;
-        int tlength = 0, twidth = 0;
-        for(int i=0;i<3;i++){
-            tlength=30;
-            for(int j=0;j<20;j++){
-                a[i][j] = new GLabel(words.charAt(cnt++)+"");
-                a[i][j].setFont(new Font("Courier", Font.PLAIN, 18));
-                a[i][j].setColor(Color.GRAY);
-                add(a[i][j],y+tlength,x+twidth);
-                tlength += a[i][j].getWidth();
-            }
-            twidth += a[i][0].getHeight();
+    private static findWords findWord;
+    static {
+        try {
+            findWord = new findWords(true,true,true);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
+    }
 
+    public void run(){
+        this.resize(1000,600);
+        ArrayList<Character>[] wordGrid = sortGrid();
+        showWords(wordGrid);
         addKeyListeners();
 
     }
 
+    // splits into lines with max of 50 characters
+    public ArrayList<Character>[] sortGrid(){
+        String[] selectedWords = findWord.getWords();
+        ArrayList<Character>[] wordGrid = new ArrayList[500];
+        for(int i=0;i<500;i++){
+            wordGrid[i] = new ArrayList<Character>();
+        }
+
+        int ypos = 0;
+        for(int i=0;i<selectedWords.length;i++){
+            if(wordGrid[ypos].size() + selectedWords[i].length() >= 50){
+                ypos++;
+            }else{
+                for(int j=0;j<selectedWords[i].length();j++){
+                    wordGrid[ypos].add(selectedWords[i].charAt(j));
+                }
+                wordGrid[ypos].add(' ');
+            }
+        }
+        gridMax = ypos;
+        return wordGrid;
+    }
+
+    public void showWords(ArrayList<Character>[] wordGrid){
+        for(int i=0;i<500;i++){
+            displayGrid[i] = new ArrayList<GLabel>();
+        }
+        int x = 80,y = 200;
+        int tlength = 0, twidth = 0;
+
+        for(int i=0;i<3;i++){
+            tlength = x;
+            for(int j=0;j<wordGrid[i].size();j++){
+                GLabel label = new GLabel(wordGrid[i].get(j)+"");
+                label.setFont(new Font("Courier", Font.PLAIN, 24));
+                label.setColor(Color.GRAY);
+                displayGrid[i].add(label);
+                add(displayGrid[i].get(j),x+tlength,y+twidth);
+                tlength += displayGrid[i].get(j).getWidth();
+            }
+            twidth += displayGrid[i].get(0).getHeight();
+        }
+    }
+
+
     public void keyPressed(KeyEvent keyPressed){
+        System.out.println(cursorX + " " + cursorY);
         switch(keyPressed.getKeyCode()){
             case KeyEvent.VK_SHIFT:
                 break;
             case KeyEvent.VK_BACK_SPACE:
-                if(posx>0){
-                    posx--;
-                    a[posy][posx].setColor(Color.GRAY);
+                if(cursorX>0){
+                    cursorX--;
+                    displayGrid[cursorY].get(cursorX).setColor(Color.GRAY);
                 }
                 break;
             default:
                 String typed = keyPressed.getKeyChar()+"";
-                if(typed.equals(a[posy][posx].getLabel())){
-                    a[posy][posx].setColor(Color.BLUE);
+                if(typed.equals(displayGrid[cursorY].get(cursorX).getLabel())){
+                    displayGrid[cursorY].get(cursorX).setColor(Color.BLUE);
                 }else{
-                    a[posy][posx].setColor(Color.RED);
+                    displayGrid[cursorY].get(cursorX).setColor(Color.RED);
                 }
-                a[posy][posx].setLabel(typed);
-                posx++;
-                if(posx>=20){
-                    posx=0;
-                    posy++;
+                displayGrid[cursorY].get(cursorX).setLabel(typed);
+                cursorX++;
+                if(cursorX>=displayGrid[cursorY].size()){
+                    cursorX=0;
+                    cursorY++;
                 }
         }
     }
+
 
     public static void main(String[] args) {
         new printwords().start(args);
