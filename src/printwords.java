1fe2314b6b44f5a2a6c14dc8101d8a2e13e1da6e
Katie <kf1030@student.cis.edu.hk>
2024-05-15 22:28:27 +0800

    new lines

--- src/printwords.java
+++ src/printwords.java
@@ -10,11 +10,15 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import java.util.Date;
+
 
 public class printwords  extends GraphicsProgram implements ActionListener {
     private static ArrayList<GLabel>[] displayGrid = new ArrayList[500];
-    int gridMax;
-    int cursorX = 0,cursorY = 0;
+    private static ArrayList<Character>[] wordGrid;
+    private static ArrayList<Character>[] curWordGrid = new ArrayList[3];
+    int gridMax, curLine=0;
+    int cursorX = 0, cursorY = 0;
 
     private static findWords findWord;
     static {
@@ -27,10 +31,9 @@ public class printwords  extends GraphicsProgram implements ActionListener {
 
     public void run(){
         this.resize(1000,600);
-        ArrayList<Character>[] wordGrid = sortGrid();
-        showWords(wordGrid);
+        wordGrid = sortGrid();
+        showWords();
         addKeyListeners();
-
     }
 
     // splits into lines with max of 50 characters
@@ -56,7 +59,7 @@ public class printwords  extends GraphicsProgram implements ActionListener {
         return wordGrid;
     }
 
-    public void showWords(ArrayList<Character>[] wordGrid){
+    public void showWords(){
         for(int i=0;i<500;i++){
             displayGrid[i] = new ArrayList<GLabel>();
         }
@@ -77,9 +80,39 @@ public class printwords  extends GraphicsProgram implements ActionListener {
         }
     }
 
+    public void updateLine(int line){
+        for(int i=0;i<displayGrid[line].size();i++){
+            remove(displayGrid[line].get(i));
+        }
+        displayGrid[line].clear();
+        for(int i=0;i<displayGrid[line+1].size();i++){
+            GLabel temp = new GLabel(displayGrid[line+1].get(i).getLabel());
+            temp.setFont(new Font("Courier", Font.PLAIN, 24));
+            temp.setColor(displayGrid[line+1].get(i).getColor());
+            temp.setLocation(displayGrid[line+1].get(i).getX(),displayGrid[line+1].get(i).getY()-29);
+            displayGrid[line].add(temp);
+            add(displayGrid[line].get(i));
+        }
+    }
+
+    public void newLine(){
+        for(int i=0;i<displayGrid[2].size();i++){
+            remove(displayGrid[2].get(i));
+        }
+        displayGrid[2].clear();
+        int x = 80;
+        int tlength = 80;
+        for(int j=0;j<wordGrid[curLine+1].size();j++){
+            GLabel label = new GLabel(wordGrid[curLine+1].get(j)+"");
+            label.setFont(new Font("Courier", Font.PLAIN, 24));
+            label.setColor(Color.GRAY);
+            displayGrid[2].add(label);
+            add(displayGrid[2].get(j),x+tlength,displayGrid[1].get(0).getY()+29);
+            tlength += displayGrid[2].get(j).getWidth();
+        }
+    }
 
     public void keyPressed(KeyEvent keyPressed){
-        System.out.println(cursorX + " " + cursorY);
         switch(keyPressed.getKeyCode()){
             case KeyEvent.VK_SHIFT:
                 break;
@@ -87,6 +120,7 @@ public class printwords  extends GraphicsProgram implements ActionListener {
                 if(cursorX>0){
                     cursorX--;
                     displayGrid[cursorY].get(cursorX).setColor(Color.GRAY);
+                    displayGrid[cursorY].get(cursorX).setLabel(wordGrid[curLine].get(cursorX)+"");
                 }
                 break;
             default:
@@ -99,8 +133,16 @@ public class printwords  extends GraphicsProgram implements ActionListener {
                 displayGrid[cursorY].get(cursorX).setLabel(typed);
                 cursorX++;
                 if(cursorX>=displayGrid[cursorY].size()){
+                    curLine++;
                     cursorX=0;
-                    cursorY++;
+                    if(cursorY==1){
+                        System.out.println("update");
+                        updateLine(0);
+                        updateLine(1);
+                        newLine();
+                    }else{
+                        cursorY++;
+                    }
                 }
         }
     }
